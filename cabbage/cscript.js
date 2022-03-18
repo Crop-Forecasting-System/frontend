1a6c1b2448cf643de7a29b5de42bd6cf6ed47242
delee3064 <delee3064@gmail.com>
2022-03-18 18:24:08 +0900

    세부페이지 차트샘플완성

--- cabbage/cscript.js
+++ cabbage/cscript.js
@@ -14,53 +14,58 @@ document.querySelector(".bg").addEventListener("click", close);
 
 //팝업창끝
 
-var sschartArea = document.getElementById('cchart').getContext('2d');
-
-var cchart = new Chart(sschartArea, {
-    type: 'line',
-    data: {
-        labels: ['전월', '현월', '다음월'],
-        datasets: [
-            {
-                label: '최대값',
-                data: [1200, 1900, 2200],
-                backgroundColor: '#e24f31',
-                borderColor: '#e24f31',
-                borderWidth: 1,
-                fill: false
-            },
-            {
-                label: '예측값',
-                data: [1000, 1600, 1500],
-                backgroundColor: '#60c0ce',
-                borderColor: '#60c0ce',
-                borderWidth: 1,
-                fill: false
-            },
-            {
-                label: '최저값',
-                data: [800, 1300, 800],
-                backgroundColor: '#18b171',
-                borderColor: '#18b171',
-                borderWidth: 1,
-                fill: false
-            }
-        ]
-    },
+var chartData = {
+    labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
+    datasets: [{
+        label: '평균값',        
+        data: [{x: 1, y: 1200}, {x: 2, y: 1300}, {x: 3, y: 1500}, {x: 4, y: 1000}, {x: 5, y: 1800}, {x: 6, y: 1400}, {x: 7, y: 1000}, {x: 8, y: 1200}, {x: 9, y: 1300}, {x: 10, y: 800}, {x: 11, y: 1100}, {x: 12, y: 1000}]
+    
+    }]
+};
+window.onload = function() {
+  var ctx = document.getElementById("canvas").getContext("2d");
+  new Chart(ctx, {
+    type: "line",
+    data: chartData,
     options: {
         legend: {
-            position:'bottom',
-            labels: {
-                boxWidth: 10
+          display: false
+        },
+        annotation: {
+          annotations: [
+            {
+              drawTime: "afterDatasetsDraw",
+              type: "line",
+              mode: "vertical",
+              scaleID: "x-axis-0",
+              value: 10,
+              borderWidth: 2,
+              borderColor: "#e24f31",
+              label: {
+                content: "현월",
+                enabled: true,
+                position: "top"
+              }
             }
+          ]
         },
         responsive: false,
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                }
-            }]
-        },
-    }
-});
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+              }
+          }],
+          xAxes: [{
+            type: 'linear',
+            ticks: {
+                  stepSize: 1,
+                  callback: function(value, index, values) {
+                      return chartData.labels[index];
+                  }
+            }
+          }]
+        }
+      }
+  });
+};
