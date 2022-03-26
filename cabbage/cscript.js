0387e3d4e59cd24c5ec5dd5569667fe6d7371c5d
delee3064 <delee3064@gmail.com>
2022-03-27 01:07:11 +0900

    세부페이지 디자인수정(차트진행중,,)

--- cabbage/cscript.js
+++ cabbage/cscript.js
@@ -6,6 +6,7 @@ var chartData = {
     
     }]
 };
+
 var hchartData = {
   labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
   datasets: [{
@@ -27,99 +28,7 @@ window.onload = function() {
   var ctx = document.getElementById("canvas").getContext("2d");
   new Chart(ctx, {
     type: "line",
-    data: chartData,
-    options: {
-        legend: {
-          display: false,
-        },
-        annotation: {
-          annotations: [
-            {
-              drawTime: "afterDatasetsDraw",
-              type: "line",
-              mode: "vertical",
-              scaleID: "x-axis-0",
-              value: 10,
-              borderWidth: 2,
-              borderColor: "#e24f31",
-              label: {
-                content: "현월",
-                enabled: true,
-                position: "top"
-              }
-            }
-          ]
-        },
-        responsive: false,
-        scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-              }
-          }],
-          xAxes: [{
-            type: 'linear',
-            ticks: {
-                  stepSize: 1,
-                  callback: function(value, index, values) {
-                      return chartData.labels[index];
-                  }
-            }
-          }]
-        }
-      }
-  });
-
-  var hctx = document.getElementById("hcanvas").getContext("2d");
-  new Chart(hctx, {
-    type: "line",
-    data: hchartData,
-    options: {
-        legend: {
-          display: false,
-        },
-        annotation: {
-          annotations: [
-            {
-              drawTime: "afterDatasetsDraw",
-              type: "line",
-              mode: "vertical",
-              scaleID: "x-axis-0",
-              value: 10,
-              borderWidth: 2,
-              borderColor: "#e24f31",
-              label: {
-                content: "현월",
-                enabled: true,
-                position: "top"
-              }
-            }
-          ]
-        },
-        responsive: false,
-        scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-              }
-          }],
-          xAxes: [{
-            type: 'linear',
-            ticks: {
-                  stepSize: 1,
-                  callback: function(value, index, values) {
-                      return chartData.labels[index];
-                  }
-            }
-          }]
-        }
-      }
-  });
-
-  var mctx = document.getElementById("mcanvas").getContext("2d");
-  new Chart(mctx, {
-    type: "line",
-    data: mchartData,
+    data: chartData, hchartData, mchartData,
     options: {
         legend: {
           display: false,
