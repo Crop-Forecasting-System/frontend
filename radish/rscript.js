768ccd693af77ae5110f54db0eb7d9f7e9708b2d
delee3064 <delee3064@gmail.com>
2022-03-29 01:53:25 +0900

    세부페이지 완성

--- radish/rscript.js
+++ radish/rscript.js
@@ -1,164 +1,93 @@
-      var chartData = {
-        labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
-        datasets: [{
-            label: '평균값',        
-            data: [{x: 1, y: 1200}, {x: 2, y: 1300}, {x: 3, y: 1500}, {x: 4, y: 1000}, {x: 5, y: 1800}, {x: 6, y: 1400}, {x: 7, y: 1000}, {x: 8, y: 1200}, {x: 9, y: 1300}, {x: 10, y: 800}, {x: 11, y: 1100}, {x: 12, y: 1000}]
-        
-        }]
-    };
-    var hchartData = {
-      labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
-      datasets: [{
+window.onload = function() {
+  var ctx = document.getElementById("canvas").getContext("2d");
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ["9월","10월","11월","12월","1월","2월","3월","4월","5월", "6월","7월","8월"],
+      datasets: [
+        {
+          label: '평균값',        
+          data: [1500,1300,1500,800,1800,1400,1000,1200,1300,800,1100,1800],
+          backgroundColor: '#60c0ce',
+          borderColor: '#60c0ce',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
+        },
+        {
           label: '상품값',        
-          data: [{x: 1, y: 1200}, {x: 2, y: 2000}, {x: 3, y: 1500}, {x: 4, y: 1000}, {x: 5, y: 1800}, {x: 6, y: 1400}, {x: 7, y: 1000}, {x: 8, y: 1200}, {x: 9, y: 1300}, {x: 10, y: 800}, {x: 11, y: 1100}, {x: 12, y: 1000}]
-      
-      }]
-    };
-    var mchartData = {
-      labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
-      datasets: [{
+          data: [2000,1700,2200,1200,2300,1800,1200,1900,2000,1200,1300,2100],
+          backgroundColor: '#e24f31',
+          borderColor: '#e24f31',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
+        },
+        {
           label: '중품값',        
-          data: [{x: 1, y: 1200}, {x: 2, y: 700}, {x: 3, y: 1500}, {x: 4, y: 1000}, {x: 5, y: 1800}, {x: 6, y: 1400}, {x: 7, y: 1000}, {x: 8, y: 1200}, {x: 9, y: 1300}, {x: 10, y: 800}, {x: 11, y: 1100}, {x: 12, y: 1000}]
+          data: [1000,900,800,400,1300,1000,800,500,600,400,900,1500],
+          backgroundColor: '#18b171',
+          borderColor: '#18b171',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
       
-      }]
-    };
-    
-    window.onload = function() {
-      var ctx = document.getElementById("canvas").getContext("2d");
-      new Chart(ctx, {
-        type: "line",
-        data: chartData,
-        options: {
-            legend: {
-              display: false,
-            },
-            annotation: {
-              annotations: [
-                {
-                  drawTime: "afterDatasetsDraw",
-                  type: "line",
-                  mode: "vertical",
-                  scaleID: "x-axis-0",
-                  value: 10,
-                  borderWidth: 2,
-                  borderColor: "#e24f31",
-                  label: {
-                    content: "현월",
-                    enabled: true,
-                    position: "top"
-                  }
-                }
-              ]
-            },
-            responsive: false,
-            scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true,
-                  }
-              }],
-              xAxes: [{
-                type: 'linear',
-                ticks: {
-                      stepSize: 1,
-                      callback: function(value, index, values) {
-                          return chartData.labels[index];
-                      }
-                }
-              }]
+        }
+      ]
+    },
+    options: {
+        legend: {
+          position: 'bottom',
+          labels: {
+            usePointStyle: true,
+          },
+        },
+        annotation: {
+          annotations: [
+            {
+              type: "line",
+              mode: "vertical",
+              scaleID: "x-axis-0",
+              value: '6월',
+              borderWidth: 2,
+              borderColor: "#F6BB43",
+              label: {
+                content: "현월",
+                enabled: true,
+                position: "top"
+              }
             }
-          }
-      });
-    
-      var hctx = document.getElementById("hcanvas").getContext("2d");
-      new Chart(hctx, {
-        type: "line",
-        data: hchartData,
-        options: {
-            legend: {
-              display: false,
-            },
-            annotation: {
-              annotations: [
-                {
-                  drawTime: "afterDatasetsDraw",
-                  type: "line",
-                  mode: "vertical",
-                  scaleID: "x-axis-0",
-                  value: 10,
-                  borderWidth: 2,
-                  borderColor: "#e24f31",
-                  label: {
-                    content: "현월",
-                    enabled: true,
-                    position: "top"
-                  }
-                }
-              ]
-            },
-            responsive: false,
-            scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true,
-                  }
-              }],
-              xAxes: [{
-                type: 'linear',
-                ticks: {
-                      stepSize: 1,
-                      callback: function(value, index, values) {
-                          return chartData.labels[index];
-                      }
-                }
-              }]
+          ]
+        },
+        responsive: false,
+        tooltips: {
+          mode: 'index',
+          intersect: true,
+          titleFontSize: 15,
+          bodyFontSize: 15
+        },
+        hover: {
+          mode: 'index',
+          intersect: true
+        },
+        scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+              }
+          }],
+          xAxes: [{
+            ticks: {
+                  beginAtZero: true,
             }
-          }
-      });
-    
-      var mctx = document.getElementById("mcanvas").getContext("2d");
-      new Chart(mctx, {
-        type: "line",
-        data: mchartData,
-        options: {
-            legend: {
-              display: false,
-            },
-            annotation: {
-              annotations: [
-                {
-                  drawTime: "afterDatasetsDraw",
-                  type: "line",
-                  mode: "vertical",
-                  scaleID: "x-axis-0",
-                  value: 10,
-                  borderWidth: 2,
-                  borderColor: "#e24f31",
-                  label: {
-                    content: "현월",
-                    enabled: true,
-                    position: "top"
-                  }
-                }
-              ]
-            },
-            responsive: false,
-            scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true,
-                  }
-              }],
-              xAxes: [{
-                type: 'linear',
-                ticks: {
-                      stepSize: 1,
-                      callback: function(value, index, values) {
-                          return chartData.labels[index];
-                      }
-                }
-              }]
-            }
-          }
-      });
-    };
+          }]
+        }
+      }
+  });
+};
