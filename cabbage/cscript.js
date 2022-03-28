3a06daf08881ffd973c40e98898e8533883d022f
delee3064 <delee3064@gmail.com>
2022-03-29 01:31:06 +0900

    차트 진짜 완성

--- cabbage/cscript.js
+++ cabbage/cscript.js
@@ -1,50 +1,46 @@
-var chartData = {
-  labels: ["9","10","11","12","1","2","3","4","5", "6","7","8"],
-  datasets: [
-    {
-      label: '평균값',        
-      data: [{x: 1, y: 1500}, {x: 2, y: 1300}, {x: 3, y: 1500}, {x: 4, y: 800}, {x: 5, y: 1800}, {x: 6, y: 1400}, {x: 7, y: 1000}, {x: 8, y: 1200}, {x: 9, y: 1300}, {x: 10, y: 800}, {x: 11, y: 1100}, {x: 12, y: 1800}],
-      backgroundColor: '#60c0ce',
-      borderColor: '#60c0ce',
-      borderWidth: 3,
-      pointStyle: 'rectRot',
-      pointRadius: 5,
-      pointHoverRadius: 8,
-      fill: false
-    },
-    {
-      label: '상품값',        
-      data: [{x: 1, y: 2000}, {x: 2, y: 1700}, {x: 3, y: 2200}, {x: 4, y: 1200}, {x: 5, y: 2300}, {x: 6, y: 1800}, {x: 7, y: 1200}, {x: 8, y: 1900}, {x: 9, y: 2000}, {x: 10, y: 1200}, {x: 11, y: 1300}, {x: 12, y: 2100}],
-      backgroundColor: '#e24f31',
-      borderColor: '#e24f31',
-      borderWidth: 3,
-      pointStyle: 'rectRot',
-      pointRadius: 5,
-      pointHoverRadius: 8,
-      fill: false
-    },
-    {
-      label: '중품값',        
-      data: [{x: 1, y: 1000}, {x: 2, y: 900}, {x: 3, y: 800}, {x: 4, y: 400}, {x: 5, y: 1300}, {x: 6, y: 1000}, {x: 7, y: 800}, {x: 8, y: 500}, {x: 9, y: 600}, {x: 10, y: 400}, {x: 11, y: 900}, {x: 12, y: 1500}],
-      backgroundColor: '#18b171',
-      borderColor: '#18b171',
-      borderWidth: 3,
-      pointStyle: 'rectRot',
-      pointRadius: 5,
-      pointHoverRadius: 8,
-      fill: false
-  
-    }
-  ]
-
-};
-
-
 window.onload = function() {
   var ctx = document.getElementById("canvas").getContext("2d");
   new Chart(ctx, {
     type: "line",
-    data: chartData,
+    data: {
+      labels: ["9월","10월","11월","12월","1월","2월","3월","4월","5월", "6월","7월","8월"],
+      datasets: [
+        {
+          label: '평균값',        
+          data: [1500,1300,1500,800,1800,1400,1000,1200,1300,800,1100,1800],
+          backgroundColor: '#60c0ce',
+          borderColor: '#60c0ce',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
+        },
+        {
+          label: '상품값',        
+          data: [2000,1700,2200,1200,2300,1800,1200,1900,2000,1200,1300,2100],
+          backgroundColor: '#e24f31',
+          borderColor: '#e24f31',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
+        },
+        {
+          label: '중품값',        
+          data: [1000,900,800,400,1300,1000,800,500,600,400,900,1500],
+          backgroundColor: '#18b171',
+          borderColor: '#18b171',
+          borderWidth: 3,
+          pointStyle: 'rectRot',
+          pointRadius: 5,
+          pointHoverRadius: 8,
+          fill: false
+      
+        }
+      ]
+    },
     options: {
         legend: {
           position: 'bottom',
@@ -58,7 +54,7 @@ window.onload = function() {
               type: "line",
               mode: "vertical",
               scaleID: "x-axis-0",
-              value: 10,
+              value: '6월',
               borderWidth: 2,
               borderColor: "#F6BB43",
               label: {
@@ -72,7 +68,9 @@ window.onload = function() {
         responsive: false,
         tooltips: {
           mode: 'index',
-          intersect: true
+          intersect: true,
+          titleFontSize: 15,
+          bodyFontSize: 15
         },
         hover: {
           mode: 'index',
@@ -85,12 +83,8 @@ window.onload = function() {
               }
           }],
           xAxes: [{
-            type: 'linear',
             ticks: {
-                  stepSize: 1,
-                  callback: function(value, index, values) {
-                      return chartData.labels[index];
-                  }
+                  beginAtZero: true,
             }
           }]
         }
